c49cba7fdfa9fb4e23c9c566d0c47df64a8536d0
Guillaume <guillaume.peltier.pro@gmail.com>
2022-01-01 18:29:33 +0100

    Ajout de Jupyterlab aux dépendances dev et réécriture des descriptions

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,42 +1,11 @@
 help = """
-Your project has been created!
-_____________________________________________________________________________
-                            ___________ _
-  \/                    __/   .::::.-'-(/-/)
-                     _/:  .::::.-' .-'\/\_`*******            __ (_))
-        \/          /:  .::::./   -._-.  d\|                 (_))_(__))
-                     /: ("'"'/    '.  (__/||             (_))__(_))--(__))
-                      \::).-'  -._  \/ \\/\|
-              __ _ .-'`)/  '-'. . '. |  (i_O
-          .-'      \       -'      '\|
-     _ _./      .-'|       '.  (    \\                           % % %
-  .-'   :      '_  \         '-'\  /|/      @ @ @               % % % %
- /      )\_      '- )_________.-|_/^\      @ @ @@@             % %\/% %
- (   .-'   )-._-:  /        \(/\'-._ `.     @|@@@@@              ..|........
-  (   )  _//_/|:  /          `\()   `\_\     |/_@@               )'-._.-._.-
-   ( (   \()^_/)_/             )/      \\    /                  /   /
-    )  _.-\\.\(_)__._.-'-.-'-.//_.-'-.-.)\-'/._                /       
-.-.-.-'   _o\ \\\     '::'   (o_ '-.-' |__\'-.-;~ ~ ~ ~ ~ ~ ~~/   /\   
-          \ /  \\\__          )_\    .:::::::.-'\            '- - -|
-     :::''':::::^)__\:::::::::::::::::'''''''-.  \                  '- - - - 
-    :::::::  '''''''''''   ''''''''''''':::. -'\  \       C. SWANSIGER
-_____':::::_____________________________________\__\_________________________
 
-If you have not done so already, create a conda environment for your new 
-project with:
+Le projet a bien été créé !
 
-cd {{cookiecutter.repo_name}}
-conda create --name {{cookiecutter.repo_name}} python=3.8
-conda activate {{cookiecutter.repo_name}}
-conda env export > environment.yml
+Pour initaliser l'environnement et installer les outils :
 
-Install your new project in your local conda environment with:
-
-pip install -e .
-
-You will need to manually add data to .gitignore to prevent it from syncing to
-version control.
-
-Don't forget to sync to GitHub. Have fun!
+   $ cd {{cookiecutter.project_name}}
+   $ poetry install
+   $ pip install -e
 """
 print(help)
