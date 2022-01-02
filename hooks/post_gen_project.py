e3c390c1259990c4293bd0af2815d8c416f89949
Guillaume <guillaume.peltier.pro@gmail.com>
2022-01-02 12:43:44 +0100

    Small fix and more info in help

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -4,8 +4,14 @@ Le projet a bien été créé !
 
 Pour initaliser l'environnement et installer les outils :
 
-   $ cd {{cookiecutter.project_name}}
+   $ cd {{cookiecutter.repo_name}}
    $ poetry install
    $ pip install -e
+
+Pour créer faire du versionning :
+   
+   $ git init -b master
+   $ git add . 
+   $ git commit -m 'initial commit'
 """
 print(help)
